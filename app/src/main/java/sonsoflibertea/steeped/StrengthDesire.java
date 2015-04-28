0b1d0f72fc2c1b8e1e588a599d8544f411126b51
jamespar147 <jamespar@bu.edu>
2015-04-28 02:21:37 -0400

    Added seek bar

--- app/src/main/java/sonsoflibertea/steeped/StrengthDesire.java
+++ app/src/main/java/sonsoflibertea/steeped/StrengthDesire.java
@@ -9,18 +9,20 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 /**
  * Created by a on 2015/4/6.
  */
-public class StrengthDesire extends Activity implements OnClickListener {
+public class StrengthDesire extends Activity implements SeekBar.OnSeekBarChangeListener {
 
-    private TextView tv_sd;
-    private Button buttonsd_light;
-    private Button buttonsd_normal;
-    private Button buttonsd_strong;
- // This was fond on the internet...
+    private TextView tv_sd;                                                                         // Declare activity label text
+    private SeekBar bar;                                                                            // Declare seekbar object
+    private TextView textProgress;                                                                  // Declare seekbar progress text
+    private Button continueButton;                                                                  // Declare continue button
+    float strengthDesire;
+                                                                                                    // This was found on the internet...
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -41,83 +43,56 @@ public class StrengthDesire extends Activity implements OnClickListener {
         }
     }
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //The super keyword is used to refer to the parent class in java
+                                                                                                    //The super keyword is used to refer to the parent class in java
         super.onCreate(savedInstanceState);
-
-        //How the activity actually looks is inside main.xml, inside the layout folder
-        setContentView(R.layout.strength_desire);
-
-
-        tv_sd = (TextView) findViewById(R.id.tv_sd);
-
-        buttonsd_light = (Button) findViewById(R.id.sd_light);
-        buttonsd_normal = (Button) findViewById(R.id.sd_normal);
-        buttonsd_strong = (Button) findViewById(R.id.sd_strong);
-
-
-		/*The buttons now have onClickListeners set, a method/function of the button class
-		 * to start a new activity/intent when pressed. In this case, pressing a button
-		 * will go to the results page.
-		 * */
-
-        buttonsd_light.setOnClickListener(this);
-        buttonsd_normal.setOnClickListener(this);
-        buttonsd_strong.setOnClickListener(this);
-
+                                                                                                    //How the activity actually looks is inside main.xml, inside the layout folder
+        setContentView(R.layout.strength_desire);                                                   // Setting layout view
+        tv_sd = (TextView) findViewById(R.id.tv_sd);                                                // Make activity instruction text
+        bar = (SeekBar)findViewById(R.id.seekBar);                                                  // Make seekbar object
+        bar.setOnSeekBarChangeListener(this);                                                       // Set seekbar listener.
+        textProgress = (TextView)findViewById(R.id.textProgress);                                   // Make progress text
+        continueButton = (Button)findViewById(R.id.continueButton);                                 // Make continue button
     }
 
     @Override
-	/*onClick is what is called when the buttons are pressed and they take in Views as arguments
-	 * as buttons are children of the view class, buttons can polymorphically be passed in. The button
-	 * that called the onClick is automatically fed in*/
-    public void onClick(View v) {
-        float StrengthDesire = 0; // Type of strength desired.. not sure if going to be used yet..
-        SharedPreferences prefs = this.getSharedPreferences("com.example.app", Context.MODE_PRIVATE);
-
-
-        //The switch statements grab the id values of the button pressed
-        switch (v.getId()) {
-
-
-            case R.id.sd_light: {
-                StrengthDesire = 0.75f;
-
-                break;
-            }
-            case R.id.sd_normal: {
-                StrengthDesire = 1.0f;
-
-                break;
-            }
-            case R.id.sd_strong: {
-                StrengthDesire = 1.25f;
-
-                break;
-            }
-
-            default: {
-                break;
-            }
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (progress <= 33) {
+            textProgress.setText("Mild");                                                           // Changes text to mild if slider within lower 1/3
         }
-        // This passes the correct arguments to the next screen and launches the next activity
-        prefs.edit().putFloat("com.example.app.strength", StrengthDesire).apply();
+        if (progress > 33 && progress <= 66) {
+            textProgress.setText("Medium");                                                         // Changes text to medium if slider within middle 1/3
+        }
+        if (progress > 66 && progress <= 100) {
+            textProgress.setText("Strong");                                                         // Changes text to medium if slider within upper 1/3
+        }
+        strengthDesire = 0.75f + (progress/200);                                                    // Adjusts strengthDesire variable based on seekbar progress
+    }
+
+    public void onStartTrackingTouch(SeekBar seekBar) {                                             // Required for some reason
+    }
+
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        seekBar.setSecondaryProgress(seekBar.getProgress());                                        // Set the shade of the previous value.
+    }
+
+    public void continueButtonClk(View v)                                                           // Ran when continueButton pressed
+    {
+        SharedPreferences prefs = this.getSharedPreferences("com.example.app",                      // Used for sharing data between activities
+                Context.MODE_PRIVATE);
+        prefs.edit().putFloat("com.example.app.strength", strengthDesire).apply();                  // This passes the correct arguments to the next screen and launches the next activity
         launchNextActivity();
     }
+
     private void launchNextActivity()
     {
-
-		/*The intent class represents an action is used to "load" activities into a variable so they can be passed in and launched from
-		 * the startActivity method. Basic intents take two arguments, the current class(.java) and the class(.java) that the app will move to
-		 *  The line below initializes an Intent named resultActivity and passes in (Main.this,Result.class) much like the this-> pointer in C++,
-		 *  the this keyword in java is used by classes to reference themselves*/
         Intent nextActivity = new Intent(StrengthDesire.this, Timer.class);
-
-
-//        Launches the new activity
+                                                                                                    /*The intent class represents an action is used to "load" activities into a variable so they can be passed in and launched from
+                                                                                                     * the startActivity method. Basic intents take two arguments, the current class(.java) and the class(.java) that the app will move to
+                                                                                                     *  The line below initializes an Intent named resultActivity and passes in (Main.this,Result.class) much like the this-> pointer in C++,
+                                                                                                     *  the this keyword in java is used by classes to reference themselves*/
+                                                                                                    // Launches the new activity
         startActivity(nextActivity);
     }
 }
